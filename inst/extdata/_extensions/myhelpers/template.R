ed725b64719b8cbf24750d5935616b2039590386
Philipp Kleer <philipp.kleer@posteo.com>
2024-11-25 14:24:01 -0300

    added renv to template.R

--- inst/extdata/_extensions/myhelpers/template.R
+++ inst/extdata/_extensions/myhelpers/template.R
@@ -1,11 +1,16 @@
 #################################################################
 #
-# Comparing general population to LGBTIQ*
+# Title
 # Author: Philipp Kleer
 # Web: https://bpkleer.github.io
-# Last change: May, 16th 2024
+# Last change:
 #
 #################################################################
+
+# Start Environment ---------
+library(renv)
+# Initialize a new renv project (creates renv.lock and renv library folder)
+renv::init()
 
 # Packages -----------
 library(tidyverse)
@@ -23,3 +28,18 @@ library(tidyverse)
 
 
 # Analysis ------
+
+# Build Renv snapshot ---------
+# Save the snapshot of the current project environment
+# This updates the renv.lock file with package versions
+renv::snapshot()
+
+# At any point, you can restore the environment using:
+# renv::restore()
+
+# Save session information for reproducibility
+session_info <- sessionInfo()
+saveRDS(session_info, file = 'sessionInfo.rds')
+
+# To deactivate the renv environment temporarily
+# renv::deactivate()
