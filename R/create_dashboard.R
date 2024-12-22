3e5ea3b8ddb47b4f41d8e53511dde5f934541ba9
Philipp Kleer <philipp.kleer@posteo.com>
2024-12-22 10:25:59 -0300

    corrected dashboard

--- R/create_dashboard.R
+++ R/create_dashboard.R
@@ -4,18 +4,21 @@
 #'
 #' @param filename The name of the created file. You don't have
 #'  to add \code{.qmd}.
+#' @param path The path were to store and build everything. Default is working
+#'  directory
 #' @param draftname The path to the draft file. If not indicated, the package
 #'  included draft file will be used.
-#' @param ext_name The name of extension to get the files from.
-#'  Default 'myhelpers'.
-#' 
+#' @param path_to_templates The path to the location of the template, css, and
+#'  other files.
+#'
 #' @returns \code{qmd}-file for a \code{dashboard} based on the template.
 #'
 #' @examples
 #' # create_dashboard(
 #' #   filename = NULL,
-#' #   draftname = '_extensions/myhelpers/draft-report.qmd',
-#' #   ext_name = 'myhelpers'
+#' #   path = './',
+#' #   draftname = 'draft-dashboard.qmd',
+#' #   path_to_templates = '/my_path_to_templates',
 #' # )
 #'
 #' @importFrom cli cli_alert_success cli_progress_step cli_alert_info
@@ -25,72 +28,103 @@
 #' @export
 create_dashboard <- function(
   filename = NULL,
-  draftname = '_extensions/myhelpers/draft-dashboard.qmd',
-  ext_name = 'myhelpers'
+  path = './',
+  draftname = 'draft-dashboard.qmd',
+  path_to_templates = '/Users/phil/Documents/templates/quarto'
 ) {
 
-if(!requireNamespace('cli')) install.packages('cli')
+  if(!requireNamespace('cli')) install.packages('cli')
 
-if (is.null(filename)) {
-  stop('You must provide a valid filename')
-}
+  if (is.null(filename)) {
+    stop('You must provide a valid filename')
+  }
 
-out_dir <- getwd()
+  out_dir <- getwd()
 
-# check for available extensions
-stopifnot('Extension not in package' = ext_name %in% c('myhelpers'))
-
-# check for existing _extensions directory
-if(!file.exists('_extensions')) dir.create('_extensions')
-cli::cli_alert_success('Created `_extensions` folder')
-
-# Create folder for recursive copying into ahead of time
-if(!file.exists(paste0('_extensions/', ext_name)))
-  dir.create(paste0('_extensions/', ext_name))
-cli::cli_alert_success('Created `_extensions/myhelpers` folder')
-
-# copy from internals
-
-cli::cli_progress_step(
-  'Copying template files and style files ...',
-  spinner = TRUE
-)
-
-# Copying _brand.yml
-file.copy('_extensions/myhelpers/_brand.yml', './')
-
-# copy my template
-file.copy(
-  from = system.file(
-    paste0(
-      'extdata/_extensions/',
-      ext_name
-    ),
-    package = 'myhelpers'
-  ),
-  to = paste0('_extensions/'),
-  overwrite = TRUE,
-  recursive = TRUE,
-  copy.mode = TRUE
-)
-
-# Create folder for recursive copying into ahead of time
-if (draftname == '_extensions/myhelpers/draft-report.qmd') {
-  file.copy(
-    from = '_extensions/myhelpers/assets/',
-    to = './',
-    recursive = TRUE
-  )
-}
-
-# create new qmd report based on skeleton
-readLines(draftname) |>
-  writeLines(
-    text = _,
-    con = paste0(filename, '.qmd', collapse = '')
+  # copy from internals
+  cli::cli_progress_step(
+    'Copying template files and style files ...',
+    spinner = TRUE
   )
 
-cli::cli_alert_success('Dashboard file created.')
-# open the new file in the editor
-# file.edit(paste0(filename, '.qmd'))
+  # Create folder for recursive copying into ahead of time
+  if (draftname == 'draft-dashboard.qmd') {
+
+    if (dir.exists(paste0(path_to_templates, '/assets'))) {
+      # copying all assets
+      file.copy(
+        from = paste0(path_to_templates, '/assets'),
+        to = path,
+        overwrite = TRUE,
+        recursive = TRUE,
+        copy.mode = TRUE
+      )
+
+      cli::cli_alert_success('Copied all assets into project directory.')
+    }
+
+    # create new qmd report based on draft or blank
+    if (file.exists(paste0(path_to_templates, '/draft-dashboard.qmd'))) {
+      readLines(paste0(path_to_templates, '/draft-dashboard.qmd')) |>
+        writeLines(
+          text = _,
+          con = paste0(path, filename, '.qmd', collapse = '')
+        )
+
+      cli::cli_alert_success('Created qmd-file based on `draft-dashboard.qmd`.')
+
+    } else {
+      cli::cli_alert_info('`draft-dashboard.qmd` does not exist in `path_to_templates`.')
+
+      readLines(draftname) |>
+        writeLines(
+          text = _,
+          con = paste0(filename, '.qmd', collapse = '')
+        )
+
+      cli::cli_alert_info('A blank qmd-file was created.')
+    }
+  } else {
+    readLines(draftname) |>
+      writeLines(
+        text = _,
+        con = paste0(filename, '.qmd', collapse = '')
+      )
+    cli::cli_alert_info('A blank qmd-file was created.')
+  }
+
+  # copying _brand.yml
+  if (file.exists(paste0(path_to_templates, '/_brand.yml'))) {
+    file.copy(
+      from = paste0(path_to_templates, '/_brand.yml'),
+      to = paste0(path, '_brand.yml'),
+      overwrite = FALSE,
+      copy.mode = TRUE,
+      copy.date = FALSE
+    )
+
+    cli::cli_alert_success('Created `_brand.yml` file')
+  } else {
+    cli::cli_alert_info(
+      'The file `_brand.yml` does not exist at `path_to_templates`!'
+    )
+  }
+
+  # copying _quarto.yml
+  if (file.exists(paste0(path_to_templates, '/_quarto.yml'))) {
+    file.copy(
+      from = paste0(path_to_templates, '/_quarto.yml'),
+      to = paste0(path, '_quarto.yml'),
+      overwrite = FALSE,
+      copy.mode = TRUE,
+      copy.date = FALSE
+    )
+
+    cli::cli_alert_success('Created `_quarto.yml` file')
+  } else {
+    cli::cli_alert_info(
+      'The file `_brand.yml` does not exist at `path_to_templates`!'
+    )
+  }
+
 }
